5f5b7ee79368c10fd3e4dafac641b4cd83bc70bf
junaed-uddin <junaed.binuddin@gmail.com>
2025-08-14 12:08:00 +0600

    protect the routes

--- routers/blog.py
+++ routers/blog.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
+from typing import Annotated
 import schemas, models
 from database import get_db
 from repositories import blog
+import oauth2
 
 router = APIRouter(
     prefix = "/blog",
@@ -12,34 +14,35 @@ router = APIRouter(
 
 # show all blogs
 @router.get("/") # response_model = List[schemas.BlogShow]
-def all_blogs(db: Session = Depends(get_db)):
+def all_blogs(current_user: Annotated[schemas.User, Depends(oauth2.get_current_user)], 
+              db: Session = Depends(get_db)):
     return blog.show_all(db)
 
 
 
 # create blog
 @router.post("/", status_code= status.HTTP_201_CREATED)
-def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
+def create_blog(current_user: Annotated[schemas.User, Depends(oauth2.get_current_user)], request: schemas.Blog, db: Session = Depends(get_db)):
     return blog.create(request, db)
 
 
 
 # show single blog
 @router.get("/{id}", status_code = 200, response_model = schemas.BlogShow) # response_model = schemas.BlogShow
-def single_blog(id: int, db: Session = Depends(get_db)):
+def single_blog(id: int, current_user: Annotated[schemas.User, Depends(oauth2.get_current_user)], db: Session = Depends(get_db)):
     return blog.show_single(id, db)
 
 
 
 # delete blog
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def blog_delete(id, db: Session = Depends(get_db)):
+def blog_delete(id: int, current_user: Annotated[schemas.User, Depends(oauth2.get_current_user)], db: Session = Depends(get_db)):
     return blog.delete(id, db)
     
     
     
 # update blog 
 @router.put("/{id}", status_code = status.HTTP_202_ACCEPTED)
-def blog_update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+def blog_update(id: int, current_user: Annotated[schemas.User, Depends(oauth2.get_current_user)], request: schemas.Blog, db: Session = Depends(get_db)):
     return blog.update(id, request, db)
  
